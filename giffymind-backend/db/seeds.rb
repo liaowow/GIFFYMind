30698f5fb51a6ba340013d4b51713471a6b14fcf
itslizkim <elizabeth.kim93@gmail.com>
2020-03-02 10:29:04 -0500

    seeded data

--- giffymind-backend/db/seeds.rb
+++ giffymind-backend/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+gracie = Gif.create(image_url: "https://ca.slack-edge.com/T02MD9XTF-UD5PW68JW-55f48a258cbc-512")
+mazen = Gif.create(image_url:"https://ca.slack-edge.com/T02MD9XTF-UFKMPLPQ8-a306271a419d-512")
+eric = Gif.create(image_url:"https://ca.slack-edge.com/T02MD9XTF-U91CXSUN4-455dd3bdbbbf-512")
+thinking = Gif.create(image_url:"https://media.giphy.com/media/CaiVJuZGvR8HK/giphy.gif")
+kissing = Gif.create(image_url:"https://media.giphy.com/media/8YBM61XW2vZJykO0j4/giphy.gif")
+screaming = Gif.create(image_url:"https://media.giphy.com/media/11CD1W0njRgJFK/giphy.gif")
+tongue_out = Gif.create(image_url:"https://media.giphy.com/media/JlfERQStnPLiM/giphy.gif")
+winking = Gif.create(image_url:"https://media.giphy.com/media/138soHYfxnxfbi/giphy.gif")
+
+annie = User.create(username: "annie")
+liz = User.create(username: "liz")
+
+g1 = Game.create(input: "", user_id: annie.id)
+g2 = Game.create(input: "", user_id: liz.id)
+
+gg1 = GameGif.create(game_id: g1.id, gif_id: gracie.id)
+gg2 = GameGif.create(game_id: g1.id, gif_id: eric.id)
+gg3 = GameGif.create(game_id: g1.id, gif_id: winking.id)
+gg4 = GameGif.create(game_id: g1.id, gif_id: mazen.id)
+
+gg5 = GameGif.create(game_id: g2.id, gif_id: kissing.id)
+gg6 = GameGif.create(game_id: g2.id, gif_id: eric.id)
+gg7 = GameGif.create(game_id: g2.id, gif_id: tongue_out.id)
+gg8 = GameGif.create(game_id: g2.id, gif_id: screaming.id)
+
+
+
